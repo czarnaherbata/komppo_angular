aab8436643d113dda7f239560c58cfdce37e5bfe
czarnaherbata <czarnaherbata223@gmail.com>
2022-01-07 12:57:17 +0100

    proba zmiany marna

--- cinema/src/app/app.module.ts
+++ cinema/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
+import { EditMovieComponent } from './edit-movie/edit-movie.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { MoviesMovieComponent } from './movies-movie/movies-movie.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
+    MovieComponent,
+    EditMovieComponent,
+    AddMovieComponent,
+    MoviesMovieComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cinema/src/app/movie/movie.component.ts
+++ cinema/src/app/movie/movie.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Movies } from '../movie-mock';
 import { Movie } from '../Movie';
+import { AddMovieComponent } from '../add-movie/add-movie.component';
+import { NotExpr } from '@angular/compiler';
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
@@ -9,10 +11,11 @@ import { Movie } from '../Movie';
 export class MovieComponent implements OnInit {
 movieList=Movies;
 headers:string[]=[];
+  newMovie: Movie;
 //selectedMovie: Movie=null;
 //selectedMovie : Movie=null;
 
-  constructor() {
+  constructor( public dialog: MatDialog) {
     this.movieList.forEach(el=>{
       const keys=Object.keys(el);
       keys.forEach(key=>{
@@ -27,6 +30,22 @@ headers:string[]=[];
   ngOnInit(): void {
   
    }
- 
+openDialog(): void{
+  let dialogRef=null;
+  dialogRef=this.dialog.open(AddMovieComponent, {
+    width: '30%',
+    data:{title: '', duration: '', year: ''}
+  });
+  dialogRef.afterClosed().subscribe((result: { year: string | number | Date; title: string; duration: string; } | undefined)=>{
+    if(result!==undefined){
+      if(result.year===''){
+        result.year=new Date();
+        
+      }
+      this.newMovie=new Movie(result.title, result.duration, new Date(result.year));
+      this.movieList.push(this.newMovie)
+    }
+  })
+} 
 
 }
